69ef2feaf6a3a8f52122d4fdf1f68cb77684b94a
Ulises Colman <38303108+Ulisescol@users.noreply.github.com>
2023-12-13 21:49:38 -0300

    Publicación de los archivos de la página web

---
+++ script.js
@@ -0,0 +1,79 @@
+let diccionario = [
+    "APPLE",
+    "HOUSE",
+    "MOUSE",
+    "YOUTH",
+    "GRADE",
+    "MOUTH"
+]
+
+let palabra = getWord()
+let intentos = 6
+
+function getWord(){
+    let min = 0
+    let max = diccionario.length
+    let i = Math.floor(Math.random() * (max - min)) + min
+    return diccionario[i]
+}
+
+
+const grid = document.getElementById("grid")
+
+const boton = document.getElementById("guess-button")
+
+boton.addEventListener("click", ()=>{
+
+    const intento = leerIntento()
+    console.clear
+
+    const row = document.createElement("div")
+    row.className = "row"
+
+    for (const i in palabra) {
+        const span = document.createElement("span")
+        span.className = "letter"
+        span.innerHTML = intento[i]
+
+        if(palabra[i] == intento[i]){
+        span.style.backgroundColor = "#79b851"
+        }
+
+        else if(palabra.includes(intento[i])){      
+            span.style.backgroundColor = "#f3c237"
+        }
+
+        else{    
+            span.style.backgroundColor = "#a4aec4"
+        }
+        row.appendChild(span)
+        
+    }
+
+    grid.appendChild(row)
+
+    if(palabra == intento){
+        terminar("GANASTE!😀")
+        return
+    }
+
+    intentos--
+
+    if(intentos == 0){
+        terminar("PERDISTE!😖")
+    }
+
+})
+
+function leerIntento(){
+    const input = document.getElementById("guess-input")
+    return input.value.toUpperCase()
+}
+
+function terminar(mensaje){
+    const input = document.getElementById("guess-input")
+    input.disabled = true
+    boton.disabled = true
+    let resultado = document.getElementById('guesses')
+    resultado.innerHTML = `<h1>${mensaje}</h1>`
+}
